8e06da5de807ce8197ba8672c4117ee47a418d44
JTNewY <niner2@naver.com>
2024-05-23 17:04:20 +0900

    헤더 수정

--- mungmoong/src/main/resources/static/js/header.js
+++ mungmoong/src/main/resources/static/js/header.js
@@ -1,9 +1,23 @@
 function toggleMenu() {
-    const auth = document.getElementById('auth');
-
-    if (auth.style.display === 'flex' || auth.style.display === '') {
-        auth.style.display = 'none';
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.display === 'flex') {
+        mobileMenu.style.display = 'none';
     } else {
-        auth.style.display = 'flex';
+        mobileMenu.style.display = 'flex';
     }
 }
+
+window.addEventListener('resize', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const nav = document.getElementById('nav');
+    const auth = document.getElementById('auth');
+    
+    if (window.innerWidth > 768) {
+        mobileMenu.style.display = 'none';
+        nav.style.display = 'flex';
+        auth.style.display = 'flex';
+    } else {
+        nav.style.display = 'none';
+        auth.style.display = 'none';
+    }
+});
