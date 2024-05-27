351e98a353d1e6058d0fd6665246b677d663fa74
wogml909 <zz909@naver.com>
2024-05-27 16:58:52 +0900

    예약페이지 매핑수정

---
+++ mungmoong/src/main/java/com/mypet/mungmoong/reserve/service/ReserveService.java
@@ -0,0 +1,22 @@
+package com.mypet.mungmoong.reserve.service;
+
+import java.util.List;
+
+import com.mypet.mungmoong.reserve.dto.Reserve;
+
+public interface ReserveService {
+       // 예약 목록
+       public List<Reserve> list() throws Exception;
+       // 예약 조회
+       public Reserve select(int date_no) throws Exception;
+       // 예약 등록
+       public Integer insert(Reserve reserve) throws Exception;
+       // 예약 수정
+       public Integer update(Reserve reserve) throws Exception;
+       // 예약 삭제
+       public Integer delete(int date_no) throws Exception;
+       
+       @Override
+       String toString();
+    
+} 
---
+++ mungmoong/src/main/java/com/mypet/mungmoong/reserve/service/ReserveServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mypet.mungmoong.reserve.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mypet.mungmoong.reserve.dto.Reserve;
+import com.mypet.mungmoong.reserve.mapper.ReserveMapper;
+
+@Service
+public class ReserveServiceImpl implements ReserveService {
+    
+    @Autowired
+    private ReserveMapper reserveMapper;
+
+    @Override
+    public List<Reserve> list() throws Exception {
+       List<Reserve> reservesList = reserveMapper.list();
+       return reservesList;
+       
+    }
+
+    @Override
+    public Reserve select(int date_no) throws Exception {
+      Reserve reserve = reserveMapper.select(date_no);
+    return reserve;
+    }
+
+    @Override
+    public Integer insert(Reserve reserve) throws Exception {
+     int result = reserveMapper.insert(reserve);
+    return result;
+    }
+
+    @Override
+    public Integer update(Reserve reserve) throws Exception {
+       int result = reserveMapper.update(reserve);
+       return result;
+    }
+
+    @Override
+    public Integer delete(int date_no) throws Exception {
+   int result = reserveMapper.delete(date_no);
+    return result;
+    }
+}
