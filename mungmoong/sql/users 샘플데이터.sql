52d7f0bd65dd697ab59d4bf06aa506590644fa63
JTNewY <niner2@naver.com>
2024-05-28 11:35:08 +0900

    DDL 변경

--- mungmoong/sql/users 샘플데이터.sql
+++ mungmoong/sql/users 샘플데이터.sql
@@ -5,8 +5,8 @@
 TRUNCATE users;
 TRUNCATE user_auth;
 
-INSERT INTO users ( user_id, password, name, birth, address, mail,phone, role )
-VALUES ( 'user', '$2a$12$TrN..KcVjciCiz.5Vj96YOBljeVTTGJ9AUKmtfbGpgc9hmC7BxQ92', '사용자', 20000101,'인주대로 1000번길','user@mail.com' , '01012341234','0' );
+INSERT INTO users ( user_id, password, name, birth, address, mail,phone, role,reg_date,upd_date,enabled )
+VALUES ( 'user', '$2a$12$TrN..KcVjciCiz.5Vj96YOBljeVTTGJ9AUKmtfbGpgc9hmC7BxQ92', '사용자', 20000101,'인주대로 1000번길','user@mail.com' , '01012341234','0',now(),now(),1 );
 
 
 INSERT INTO user_auth ( user_id,  auth ) VALUES ( 'user', 'ROLE_USER' );
