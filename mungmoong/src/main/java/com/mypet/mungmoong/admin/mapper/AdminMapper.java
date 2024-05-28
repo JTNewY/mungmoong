c37c331f2868f79452cf2209a1f411b664fda3de
ltw0781 <ltw0781@naver.com>
2024-05-28 15:13:15 +0900

    2024-05-28 점심먹고 와서 커밋
    1. 컨트롤러 매퍼등 추가 및 css 수정

---
+++ mungmoong/src/main/java/com/mypet/mungmoong/admin/mapper/AdminMapper.java
@@ -0,0 +1,8 @@
+package com.mypet.mungmoong.admin.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdminMapper {
+    
+}
---
+++ mungmoong/src/main/resources/static/js/admin.js
@@ -0,0 +1,3 @@
+// function moveBoardToList() {
+//     location.href = '/admin/'
+// }
