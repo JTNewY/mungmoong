02f0461b98144f5651fb4a5c17fd7cb654cf0216
wogml909 <zz909@naver.com>
2024-06-01 23:01:46 +0900

    20240601[재희]예약 수정중

--- mungmoong/src/main/java/com/mypet/mungmoong/main/controller/MainController.java
+++ mungmoong/src/main/java/com/mypet/mungmoong/main/controller/MainController.java
@@ -113,9 +113,28 @@ public class MainController {
             log.info("주소 : " + address);
         }
 
-        return "reserve/QnA";
+        return "QnA";
     }
+    @GetMapping("trainermain")
+    public String trainerMain(Principal principal
+                     ,Authentication auth) {
+        log.info("메인");
+        log.info(":::::::::: 메인 ::::::::::");
+        log.info("principal  : " + principal);
+        
+        if(principal != null ) {
+            CustomUser loginUser = (CustomUser) auth.getPrincipal();
+            String userId = loginUser.getUser().getUserId(); 
+            String address = loginUser.getUser().getAddress();
+            
+            // String userId = principal.getName();
+            log.info("auth  : " + auth.getPrincipal());
+            log.info("로그인 아이디 : " + userId);
+            log.info("주소 : " + address);
+        }
 
+        return "trainermain";
+    }
     // @GetMapping("/{page}")
     // public String test(@PathVariable("page") String page) {
     //     return "/" + page;
