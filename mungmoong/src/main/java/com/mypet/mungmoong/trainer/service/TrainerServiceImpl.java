71f7c1d573f105dd98233d020b12550e28e6a8aa
OoooEuna <160719463+OoooEuna@users.noreply.github.com>
2024-06-07 10:17:09 +0900

    커밋

--- mungmoong/src/main/java/com/mypet/mungmoong/trainer/service/TrainerServiceImpl.java
+++ mungmoong/src/main/java/com/mypet/mungmoong/trainer/service/TrainerServiceImpl.java
@@ -208,10 +208,7 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public int selectTrainerNo() throws Exception {
-<<<<<<< HEAD
-=======
         // TODO Auto-generated method stub
->>>>>>> 7bb94a0f536237828d7448ceb9e6c058894b9295
         throw new UnsupportedOperationException("Unimplemented method 'selectTrainerNo'");
     }
 
