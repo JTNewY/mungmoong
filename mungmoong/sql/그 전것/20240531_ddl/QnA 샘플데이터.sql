56b3189296f5605239012bf553e4471831d71d04
oeuna <oeuna6867@gmail.com>
2024-06-08 17:29:50 +0900

    trainer.sql 정리 및 필요없는거쳐내기

--- mungmoong/sql/그 전것/20240531_ddl/QnA 샘플데이터.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO qna (title, content, fileName, userId, qnaType)
-VALUES ('제목 예시', '내용 예시', '파일이름 예시', '사용자ID 예시', 1);
-
-INSERT INTO qna (title, content, fileName, userId, qnaType)
-VALUES ('또 다른 제목', '또 다른 내용', '다른파일이름', '다른사용자ID', 2);
